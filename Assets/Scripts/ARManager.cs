2c709a990fcee4073f9aa8de3d79fdd2e9a701f1
xmasbasnet <51314234+xmasbasnet@users.noreply.github.com>
2023-07-30 21:57:01 +0545

    Initialize Project
    Project Added

---
+++ Assets/Scripts/ARManager.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class ARManager : MonoBehaviour
+{
+    
+}
---
+++ Assets/Scripts/ButtonItem.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonItem : MonoBehaviour
+{
+    private int index;
+
+
+    public void SetButton(int i)
+    {
+        index = i;
+        transform.GetComponent<Button>().onClick.AddListener(() => OnButtonClicked());
+    }
+
+    private void OnButtonClicked()
+    {
+        AppManager.instance.PressedItemButton(index,this);
+    }
+
+    public void EnableSelectedUI() {
+        transform.GetChild(1).gameObject.SetActive(true);
+    }
+
+    public void DisableSelectedUI()
+    {
+        transform.GetChild(1).gameObject.SetActive(false);
+
+    }
+
+
+}
